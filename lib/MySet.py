ffba0d31879faedae3d0071b9ab819aa6faa2dad
Marysah <Marysah2@gmail.com>
2025-12-09 23:59:53 +0300

    initial commmit

--- lib/MySet.py
+++ lib/MySet.py
@@ -1,3 +1,30 @@
-class MySet:
 
-    pass
+
+class MySet:
+    def __init__(self, enumerable=None):
+        self.dictionary = {}
+        if enumerable is not None:
+            for item in enumerable:
+                self.dictionary[item] = True
+
+    def has(self, value):
+        return value in self.dictionary
+
+    def add(self, value):
+        self.dictionary[value] = True
+        return self
+
+    def delete(self, value):
+        if value in self.dictionary:
+            del self.dictionary[value]
+            return True
+        return False
+
+    def size(self):
+        return len(self.dictionary)
+
+    def clear(self):
+        self.dictionary = {}
+
+    def __str__(self):
+        return f"{{{', '.join(str(k) for k in self.dictionary.keys())}}}"
